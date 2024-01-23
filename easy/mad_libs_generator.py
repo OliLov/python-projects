f9aee5d751852d2f26dd9df466efc79675ff2e78
Oliver Lövström <oliver.lovstrom@gmail.com>
2024-01-23 18:27:19 +0100

    Add mad libs generator

---
+++ easy/mad_libs_generator.py
@@ -0,0 +1,15 @@
+"""Mad libs generator."""
+# pylint: disable=E0401
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, pipeline
+
+MODEL_NAME = "gpt2"
+
+model = GPT2LMHeadModel.from_pretrained(MODEL_NAME)
+tokenizer = GPT2Tokenizer.from_pretrained(MODEL_NAME)
+
+user_input = input("Enter prompt: ")
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+story = generator(user_input, max_length=100)
+
+print(f"Story: {story[0]['generated_text']}")
